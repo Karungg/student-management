09e423b839f9fcfaa5631d79412db702b54cfdeb
Karungg <miftahfadilah71@gmail.com>
2025-04-28 20:10:08 +0700

    feat(backend): create Student model

---
+++ backend/src/main/java/com/student/Model/Student.java
@@ -0,0 +1,77 @@
+package com.student.Model;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String nim;
+
+    private int age;
+    private String email;
+
+    public Student() {
+    }
+
+    public Student(String name, String nim, int age, String email) {
+        this.name = name;
+        this.nim = nim;
+        this.age = age;
+        this.email = email;
+    }
+
+    public UUID getId() {
+        return this.id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNim() {
+        return this.nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
