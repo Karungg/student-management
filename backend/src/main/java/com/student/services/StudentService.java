20f9e27cd660b4b89ddeb623e6abe7ec494ac5c5
Karungg <miftahfadilah71@gmail.com>
2025-04-28 20:18:43 +0700

    feat(backend): create StudentService class

---
+++ backend/src/main/java/com/student/services/StudentService.java
@@ -0,0 +1,42 @@
+package com.student.services;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.student.model.Student;
+import com.student.repository.StudentRepository;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    public Optional<Student> getStudentById(UUID id) {
+        return studentRepository.findById(id);
+    }
+
+    public Student addStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public Student updaStudent(UUID id, Student studentDetails) {
+        if (studentRepository.existsById(id)) {
+            studentDetails.setId(id);
+            return studentRepository.save(studentDetails);
+        }
+        return null;
+    }
+
+    public void deleteStudent(UUID id) {
+        studentRepository.deleteById(id);
+    }
+}
