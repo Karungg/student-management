cd3b4b01bbfca5272757ad24dda9955a538accc7
Karungg <miftahfadilah71@gmail.com>
2025-04-29 19:31:12 +0700

    feat(frontend): create AddStudent.js

---
+++ frontend/src/pages/AddStudent.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import StudentForm from "../components/StudentForm";
+import api from "../api";
+
+const AddStudent = () => {
+  const navigate = useNavigate();
+
+  const handleSubmit = (data) => {
+    api.post("/", data).then(() => navigate("/"));
+  };
+
+  return <StudentForm onSubmit={handleSubmit} />;
+};
+
+export default AddStudent;
